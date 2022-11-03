7cb9d82a487452d7bd1db7f439edb543b7673dc4
cjk327 <cjk327@drexel.edu>
2022-11-02 20:53:25 -0400

    main pages

---
+++ final/search.php
@@ -0,0 +1,78 @@
+<?php 
+$title = "Search";
+$header = "Search";
+$headerText = "Search through our delicious and health concious recipes!";
+include_once 'includes/header.php';
+?>
+
+        <div class="search">
+            <img src="images/search.png" alt="Search Icon" class="search-icon">
+            <input type="text" name="" placeholder="Search . . .">
+        </div>
+    </header>
+
+    <div class="recipe-container">
+        <div class="recipe-row">
+            <div class="recipe recipe-1">
+                <img src="images/the-classic.png" alt="The Classic" class="recipe-img">
+                <div class="top-line">
+                    <h3>The Classic</h4>
+                    <p>500 cals.</p>
+                </div>
+                <p>Cook Time . . . . . . . . . . . . . . . . . . . 10 mins</p>
+            </div>
+
+            <div class="recipe recipe-1">
+                <img src="images/the-classic.png" alt="The Classic" class="recipe-img">
+                <div class="top-line">
+                    <h3>The Classic</h4>
+                    <p>500 cals.</p>
+                </div>
+                <p>Cook Time . . . . . . . . . . . . . . . . . . . 10 mins</p>
+            </div>
+
+            <div class="recipe recipe-1">
+                <img src="images/the-classic.png" alt="The Classic" class="recipe-img">
+                <div class="top-line">
+                    <h3>The Classic</h4>
+                    <p>500 cals.</p>
+                </div>
+                <p>Cook Time . . . . . . . . . . . . . . . . . . . 10 mins</p>
+            </div>
+
+            <div class="recipe recipe-1">
+                <img src="images/the-classic.png" alt="The Classic" class="recipe-img">
+                <div class="top-line">
+                    <h3>The Classic</h4>
+                    <p>500 cals.</p>
+                </div>
+                <p>Cook Time . . . . . . . . . . . . . . . . . . . 10 mins</p>
+            </div>
+
+            <div class="recipe recipe-1">
+                <img src="images/the-classic.png" alt="The Classic" class="recipe-img">
+                <div class="top-line">
+                    <h3>The Classic</h4>
+                    <p>500 cals.</p>
+                </div>
+                <p>Cook Time . . . . . . . . . . . . . . . . . . . 10 mins</p>
+            </div>
+
+            <div class="recipe recipe-1">
+                <img src="images/the-classic.png" alt="The Classic" class="recipe-img">
+                <div class="top-line">
+                    <h3>The Classic</h4>
+                    <p>500 cals.</p>
+                </div>
+                <p>Cook Time . . . . . . . . . . . . . . . . . . . 10 mins</p>
+            </div>
+        </div>
+
+    </div>      
+
+</body>
+</html>
+
+<?php 
+include_once 'includes/footer.php'; 
+?>
