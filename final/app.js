e926b6803a8392b05a6c85198ee47effd3789e0f
cjk327 <cjk327@drexel.edu>
2022-11-02 20:53:44 -0400

    js for nav

--- final/app.js
+++ final/app.js
@@ -1,1 +1,11 @@
-console.log("script loaded");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+
+    // //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
