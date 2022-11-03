a59cec50cc71652cdd965660dce7e3f46e8484b3
cjk327 <cjk327@drexel.edu>
2022-11-02 20:53:39 -0400

    global includes

--- final/includes/footer.php
+++ final/includes/footer.php
@@ -1,2 +1,7 @@
+<script src="./app.js"></script>
 </body>
+
+<footer>
+    <h3 class="footer-text">CarbSense 2022</h3>
+</footer>
 </html>
--- final/includes/header.php
+++ final/includes/header.php
@@ -5,6 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>IDM 232 - Final Project</title>
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Impact' rel='stylesheet'>
+    <title><?php echo"$title"; ?></title>
 </head>
 <body>
+<nav>
+        <div class="logo">
+            <h2 class="logo-text">CarbSense</h2>
+        </div>
+        <div class="hamburger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="./index.php">Categories</a></li>
+            <li><a href="./search.php">Search</a></li>
+            <li><button class="login-button" href="#">Login</button></li>
+        </ul>
+    </nav>
+    <header>
+        <h1><?php echo"$header"; ?></h1>
+        <h2><?php echo"$headerText"; ?></h2>
