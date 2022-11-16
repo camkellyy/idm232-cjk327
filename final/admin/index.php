05aa55bd44a7759d67a772e9ac7932bbbe950521
cjk327 <cjk327@drexel.edu>
2022-11-16 12:21:50 -0500

    admin

---
+++ final/admin/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once __DIR__ . '/../app.php';
+$title = "Home";
+$header = "";
+$headerText = "";
+include_once __DIR__ . '/../_components/header.php';
+?>
+
+
+<?php include_once __DIR__ . '/../_components/footer.php';
